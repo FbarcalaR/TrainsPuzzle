eba61049236357b772cf5c0bd582a1eabf5ba1a9
Fernando <fernandobarcalarodriguez@gmail.com>
2021-02-09 23:26:11 +0100

    train movement

--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -4,40 +4,92 @@ using System.Linq;
 using UnityEngine;
 
 public class Train : MonoBehaviour {
-    public bool CanMove { get; set; } = false;
+    public enum outterGridStartingPosition {up, down, left, right}
+    public outterGridStartingPosition outPosition;
+    public Vector2Int gridStartingPosition;
+    public float trainSpeed = 1.5f;
     public ObjectInstantiator objectInstantiator;
 
-    private List<Vector2Int> path;
-    private float timer = 0;
+    private List<PathPositionAngles> path;
+    private Rigidbody2D rigidbody2d;
+    private Vector3 targetPosition;
+    private Vector2Int outGridStartingPosition;
 
+    public bool CanMove { get; set; } = false;
 
-    // Start is called before the first frame update
     void Start() {
-        path = new List<Vector2Int>();
+        path = new List<PathPositionAngles>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        targetPosition = transform.position;
+
+        switch (outPosition) {
+            case outterGridStartingPosition.up:
+                outGridStartingPosition = Vector2Int.up;
+                break;
+            case outterGridStartingPosition.down:
+                outGridStartingPosition = Vector2Int.down;
+                break;
+            case outterGridStartingPosition.left:
+                outGridStartingPosition = Vector2Int.left;
+                break;
+            case outterGridStartingPosition.right:
+                outGridStartingPosition = Vector2Int.right;
+                break;
+            default:
+                break;
+        }
     }
 
-    // Update is called once per frame
     void Update() {
-        timer += Time.deltaTime;
-        if (CanMove && timer>=0.5f && path.Count > 0) {
-            var position = path.First();
+        if (CanMove && path.Count > 0 && TargetPositionReached()) {
+            var positionAngles = path.First();
             path.RemoveAt(0);
 
-            var newPosition = objectInstantiator.instantiatedObjects[position.x, position.y].transform.position;
-            transform.position = newPosition;
-            timer = 0;
+            var nextPosition = objectInstantiator.instantiatedObjects[positionAngles.position.x, positionAngles.position.y].transform.position;
+
+            Vector3 targetVelocity = (nextPosition - targetPosition) * 5f;
+            targetVelocity.Normalize();
+            rigidbody2d.velocity = targetVelocity * trainSpeed;
+            rigidbody2d.rotation += positionAngles.angles;
+
+            targetPosition = nextPosition;
         }
+
+        if(path.Count == 0 && TargetPositionReached()) {
+            CanMove = false;
+            rigidbody2d.angularVelocity = 0;
+            rigidbody2d.velocity = Vector3.zero;
+        }
+    }
+
+    private bool TargetPositionReached() {
+        return Vector3.Distance(targetPosition, transform.position) <= 0.1f;
     }
 
     public void UpdatePath() {
-        var currentPosition = new Vector2Int(0, 4);
-        var currentTransform = objectInstantiator.instantiatedObjects[currentPosition.x, currentPosition.y];
-        while (IsPositionValidForPath(currentTransform)) {
-            path.Add(currentPosition);
-            var nextPosition = GetNextPosition(currentPosition, currentTransform.GetComponent<Rail>());
-            currentPosition = nextPosition.GetValueOrDefault();
-            currentTransform = currentPosition != null ? objectInstantiator.instantiatedObjects[currentPosition.x, currentPosition.y] : null;
+        Vector2Int lastPosition = gridStartingPosition + outGridStartingPosition;
+        Vector2Int? currentPosition = gridStartingPosition;
+        Vector2Int? nextPosition;
+        Transform currentTransform = objectInstantiator.instantiatedObjects[gridStartingPosition.x, gridStartingPosition.y];
+        float anglesToTurn = 0;
+
+        while (IsPositionValidForPath(currentTransform) && currentPosition.HasValue) {
+            path.Add(new PathPositionAngles(currentPosition.Value, anglesToTurn));
+
+            nextPosition = GetNextPosition(currentPosition.Value, currentTransform.GetComponent<Rail>());
+            anglesToTurn = GetAnglesDifference(lastPosition, currentPosition.Value, nextPosition);
+            lastPosition = currentPosition.Value;
+            currentPosition = nextPosition ?? null;
+            currentTransform = currentPosition != null ? objectInstantiator.instantiatedObjects[currentPosition.Value.x, currentPosition.Value.y] : null;
         }
+    }
+
+    private float GetAnglesDifference(Vector2Int? lastPosition, Vector2Int currentPosition, Vector2Int? nextPosition) {
+        if (!lastPosition.HasValue || !nextPosition.HasValue) return 0;
+        Vector2Int firstVector = currentPosition - lastPosition.Value;
+        Vector2Int secondVector = nextPosition.Value - currentPosition;
+        float targetAngle = Vector2.SignedAngle(firstVector, secondVector);
+        return targetAngle;
     }
 
     private bool IsPositionValidForPath(Transform currentTransform) {
@@ -62,13 +114,24 @@ public class Train : MonoBehaviour {
         return null;
     }
 
-    private bool DirectionIsValid(Vector2Int nextPosition, Rail rail, Func<Rail, bool> p) {
-        return p.Invoke(rail) 
+    private bool DirectionIsValid(Vector2Int nextPosition, Rail rail, Func<Rail, bool> func) {
+        return func.Invoke(rail) 
             && IsMatrixPositionInBounds(nextPosition.x, nextPosition.y, objectInstantiator.instantiatedObjects)
-            && !path.Contains(nextPosition);
+            && !path.Select(p=>p.position).Contains(nextPosition);
     }
 
     private bool IsMatrixPositionInBounds(int x, int y, Transform[,] matrix) {
         return x >= 0 && y >= 0 && x < matrix.GetLength(0) && y < matrix.GetLength(1);
     }
+
+    private struct PathPositionAngles {
+        public Vector2Int position;
+        public float angles;
+
+        public PathPositionAngles(Vector2Int position, float angles) {
+            this.position = position;
+            this.angles = angles;
+        }
+    }
+
 }
