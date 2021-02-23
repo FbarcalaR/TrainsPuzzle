1bcd9ab444d5ef8bc752ce06f622a1aa753fe416
Fernando <fernandobarcalarodriguez@gmail.com>
2021-02-23 20:02:33 +0100

    added level 2

---
+++ Assets/Scripts/UI/SetAllTrainsActive.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SetAllTrainsActive : MonoBehaviour
+{
+    public void AllTrainsCanMove() {
+        var allTrains = FindObjectsOfType<Train>();
+        foreach (var train in allTrains) {
+            train.CanMove = true;
+        }
+    }
+}
