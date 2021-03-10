b66a10d2cae25a40ba3ea302831baab7636c241c
Fernando <fernandobarcalarodriguez@gmail.com>
2021-03-10 22:01:08 +0100

    Camera aspect

---
+++ Assets/Scripts/UI/MyCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+     
+[ExecuteInEditMode]
+public class MyCamera : MonoBehaviour {
+    private Camera m_camera;
+
+    [SerializeField]
+    private float m_orthographicSize = 5f;
+
+    private void Start() {
+        RefreshCamera();
+    }
+
+    public void RefreshCamera() {
+        if (m_camera == null)
+            m_camera = GetComponent<Camera>();
+
+        AdjustCamera(m_camera.aspect);
+    }
+
+    private void AdjustCamera(float aspect) {
+        float _1OverAspect = 1f / aspect;
+        m_camera.orthographicSize = m_orthographicSize * _1OverAspect;
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate() {
+        RefreshCamera();
+    }
+#endif
+}
