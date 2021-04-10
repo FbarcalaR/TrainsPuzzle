696d2fa326575fcc21c4d2584bb965fd39a9aa3d
Fernando <fernandobarcalarodriguez@gmail.com>
2021-04-10 13:12:03 +0200

    manage and save completed levels

---
+++ Assets/Scripts/GameManagement/CompletedLevelsManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class CompletedLevelsManager : MonoBehaviour {
+    private List<int> completedLevels;
+
+    private string SaveFilePath => Application.persistentDataPath + "/gamesave.save";
+
+    private void Awake() {
+        LoadCompletedLevels();
+    }
+
+    public void AddCompletedLevel(int level) {
+        completedLevels.Add(level);
+        SaveCompletedLevels();
+    }
+
+    public bool IsLevelCompleted(int level) {
+        return completedLevels.Contains(level);
+    }
+
+    public void SaveCompletedLevels() {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(SaveFilePath);
+        bf.Serialize(file, completedLevels);
+        file.Close();
+    }
+
+    public void LoadCompletedLevels() {
+        if (File.Exists(SaveFilePath)) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(SaveFilePath, FileMode.Open);
+            completedLevels = (List<int>) bf.Deserialize(file);
+            file.Close();
+            Debug.Log("Game Loaded");
+        }
+        else {
+            completedLevels = new List<int>();
+            Debug.Log("No game saved!");
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/InteractableButtonsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableButtonsManager : MonoBehaviour {
+    public List<Button> buttons;
+    public CompletedLevelsManager completedLevelsManager;
+
+    void Start() {
+        for (int i = 2; i < buttons.Count; i++) {
+            bool completed = completedLevelsManager.IsLevelCompleted(i);
+            if (!completed) {
+                buttons[i].interactable = false;
+            }
+        }
+    }
+}
